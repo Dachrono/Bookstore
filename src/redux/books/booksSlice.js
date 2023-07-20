2ec2d617b5378ba56665fd974a80477cc4cbc08a
Dachrono <andres_zamorano_@hotmail.com>
2023-07-19 22:02:28 -0600

    Use of <provider> component with store, Create initial state for booksSlice, Display new data books, use of dispatch actions, Add function to add and erase buttons

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const booksData = [
+  {
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
+
 const initialState = {
-  books: [],
+  books: booksData,
 };
 
 const booksSlice = createSlice({
@@ -12,7 +33,7 @@ const booksSlice = createSlice({
       state.books.push(action.payload);
     },
     removeBook: (state, action) => {
-      state.books = state.books.filter((book) => book.id !== action.payload);
+      state.books = state.books.filter((book) => book.item_id !== action.payload);
     },
   },
 });
